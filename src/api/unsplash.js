90e798d62abeee5f14382c28239851642c440c9d
ruro122020 <ruro122020@gmail.com>
2021-09-21 13:04:52 -0600

    updating app

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization: "Client-ID efva5_rSEckzb5fCdhHQVtEgkEt3ojSUzbx93inDblM",
+    Authorization: process.env.REACT_APP_KEY,
   },
 });
